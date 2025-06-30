e0458aca95bb463cf1ffa7eb19a7f75f0ca271d0
Harish Kumar <harishthakur8930@gmail.com>
2025-06-30 09:43:24 +0530

    context provider

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,12 +1,13 @@
 "use client";
+import { useTheme } from "@/components/theme-provider";
 
 import ImageCarousel from "@/components/imagecarousel";
 
 export default function ClientRoute() {
+  const theme = useTheme();
   return (
     <div>
-      <h1>Client Route</h1>;
-      <ImageCarousel />
+      <h1 style={{ color: theme.colors.secondary }}>Client Route</h1>;
     </div>
   );
 }
