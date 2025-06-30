40814d33cd7013e3fa091781ec5c27ef8dff2e03
Harish Kumar <harishthakur8930@gmail.com>
2025-06-30 09:55:55 +0530

    client only code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { useTheme } from "@/components/theme-provider";
-
-import ImageCarousel from "@/components/imagecarousel";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoute() {
   const theme = useTheme();
+  const result = clientSideFunction();
   return (
     <div>
       <h1 style={{ color: theme.colors.secondary }}>Client Route</h1>;
+      <p>{result}</p>
     </div>
   );
 }
